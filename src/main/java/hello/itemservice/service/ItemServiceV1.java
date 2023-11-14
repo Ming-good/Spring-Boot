6f9f43668cfb3edb3878fe4b11be3f523caf65ac
이민규 <read1516@naver.com>
2023-11-14 17:45:19 +0900

    [섹션2] JdbcTemplate 적용3 - 구성과 실행

--- src/main/java/hello/itemservice/service/ItemServiceV1.java
+++ src/main/java/hello/itemservice/service/ItemServiceV1.java
@@ -4,11 +4,10 @@ import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
